fd201e6e9f0a8872923abd237ec1189eeb0cc31c
ral1239 <ral1239@gmail.com>
2025-06-12 10:14:51 -0400

    Increased Alert font size. Add scroll for multiple alerts

--- client/directives/alertBanner/alertBanner.directive.js
+++ client/directives/alertBanner/alertBanner.directive.js
@@ -5,8 +5,8 @@ angular.module('transitScreenApp')
     return {
       template: '<div class="alert-banner" ng-if="ctrl.hasAlerts()">' +
                 '  <div class="alert-content">' +
-                '    <i class="alert-icon">⚠️</i>' +
-                '    <span ng-repeat="msg in ctrl.getAlertMessages() track by $index">{{msg}}<span ng-if="!$last">, </span></span>' +
+                '    <span class="alert-icon">⚠️</span>' +
+                '    <span class="alert-message">{{ctrl.currentAlert}}</span>' +
                 '  </div>' +
                 '</div>',
       restrict: 'E',
@@ -19,18 +19,70 @@ angular.module('transitScreenApp')
     };
   });
 
-function AlertBannerCtrl() {
+function AlertBannerCtrl($interval, $scope, ScreenConfig) {
   var vm = this;
+  var alertInterval;
+  var ALERT_DURATION = 20000; // 20 seconds
 
   angular.extend(vm, {
     hasAlerts: hasAlerts,
-    getAlertMessages: getAlertMessages
+    getAlertMessages: getAlertMessages,
+    currentAlert: '',
+    currentIndex: 0
+  });
+
+  // Watch for changes in routes
+  $scope.$watch('ctrl.routes', function(newRoutes) {
+    if (newRoutes) {
+      initAlerts();
+    }
+  }, true);
+
+  // Watch for changes in hidden routes
+  $scope.$watch(function() {
+    return ScreenConfig.hiddenRoutes;
+  }, function() {
+    initAlerts();
+  }, true);
+
+  function initAlerts() {
+    // Clean up existing interval if any
+    if (alertInterval) {
+      $interval.cancel(alertInterval);
+    }
+
+    if (hasAlerts()) {
+      var alerts = getAlertMessages();
+      if (alerts.length > 0) {
+        vm.currentAlert = alerts[0];
+        vm.currentIndex = 0;
+        
+        // Start cycling through alerts
+        alertInterval = $interval(function() {
+          vm.currentIndex = (vm.currentIndex + 1) % alerts.length;
+          vm.currentAlert = alerts[vm.currentIndex];
+        }, ALERT_DURATION);
+      } else {
+        // If no alerts after filtering, clear the current alert
+        vm.currentAlert = '';
+      }
+    } else {
+      // If no alerts at all, clear the current alert
+      vm.currentAlert = '';
+    }
+  }
+
+  // Clean up on destroy
+  $scope.$on('$destroy', function() {
+    if (alertInterval) {
+      $interval.cancel(alertInterval);
+    }
   });
 
   function hasAlerts() {
     if (!vm.routes) return false;
     return vm.routes.some(function(route) {
-      return route.alerts && route.alerts.length > 0;
+      return isRouteVisible(route) && route.alerts && route.alerts.length > 0;
     });
   }
 
@@ -38,7 +90,7 @@ function AlertBannerCtrl() {
     if (!vm.routes) return [];
     var allAlerts = [];
     vm.routes.forEach(function(route) {
-      if (route.alerts && route.alerts.length > 0) {
+      if (isRouteVisible(route) && route.alerts && route.alerts.length > 0) {
         route.alerts.forEach(function(alert) {
           if (!allAlerts.some(function(existing) {
             return existing.title === alert.title;
@@ -49,5 +101,9 @@ function AlertBannerCtrl() {
       }
     });
     return allAlerts.map(function(alert) { return alert.title; });
+  }
+
+  function isRouteVisible(route) {
+    return ScreenConfig.hiddenRoutes.indexOf(route.global_route_id) === -1;
   }
 } 
